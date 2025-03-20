0d85bf7e16880555cfcc153c947805ec5bdd04a4
Mr.Igor <iv_mirsoft@ukr.net>
2025-03-20 20:34:36 +0200

    finished with mobile menu

---
+++ src/script_js/mobile-menu.js
@@ -0,0 +1,21 @@
+var myBurgerIcon = document.getElementById("burger-icon");
+var btnOpenMenu = document.getElementById("open_menu");
+
+var myModalMenu = document.getElementById("modal_menu");
+
+function toggleClass(objForm) {
+	console.log(objForm);
+
+	return function () {
+		objForm.classList.toggle("is-open");
+
+		if (objForm.classList.contains("is-open")) {
+			myBurgerIcon.setAttribute("href", "/img/sprite.svg#icon-close-iscon");
+		} else {
+			myBurgerIcon.setAttribute("href", "/img/sprite.svg#icon-menu-icon");
+		}
+
+	};
+}
+
+btnOpenMenu.addEventListener("click", toggleClass(myModalMenu));
